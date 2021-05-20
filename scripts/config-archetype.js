6ee616fe7598da3ac3e97d4cc44f0567ea54a9bb
Javier Sanchez Ortiz <jsanchor@mercadona.es>
2021-05-20 09:11:33 +0000

    fix: Fixed scope and self-script destroy (FWKA-3210)

--- scripts/config-archetype.js
+++ scripts/config-archetype.js
@@ -1,5 +1,6 @@
 const { join } = require('path');
-const { readFile, writeFile } = require('fs/promises');
+const fs = require('fs');
+const { readFile, writeFile } = require('fs').promises;
 
 const log = (...m) => console.log(...m);
 const errorAndClose = (...m) => {
@@ -34,6 +35,14 @@ const YOUR_APP_NAME_FILES = [
   'src/environments/environment.ts'
 ].map((rootPath) => getRelativePath('../' + rootPath));
 
+const FILES_TO_AUTODESTROY = [
+  'scripts/config-archetype.js',  
+].map((rootPath) => getRelativePath('../' + rootPath));
+
+const DIRS_TO_AUTODESTROY = [  
+  'scripts'
+].map((rootPath) => getRelativePath('../' + rootPath));
+
 async function replaceFiles(filepaths, oldText, newText) {
   let filesReplaced = [];
   let filesError = [];
@@ -54,7 +63,7 @@ async function replaceFiles(filepaths, oldText, newText) {
 
 async function main(...args) {
   const config = parseArguments(...args);
-  const appName = config.appName;
+  const appName = sanitizeAppName(config.appName);
   if (!appName) errorAndClose('Mandatory argument --appName not provided');
   const scope = config.scope;
   const currentAppName = config.reset ? appName : 'yourAppName';
@@ -70,11 +79,7 @@ async function main(...args) {
   log(`Changes in package.json (name, version)`);
   const packagePath = getRelativePath('../package.json');
   const packageContent = await getFileJson(packagePath);
-  const newPackageName = config.reset
-    ? '@mercadona-fwk-front/arquetype'
-    : scope
-    ? `${scope}/${appName}`
-    : appName;
+  const newPackageName = getPackageName(config, scope, appName);
   const newVersion = config.reset
     ? packageContent.dependencies['@mercadona-fwk-front/core']
     : '0.0.1';
@@ -83,6 +88,27 @@ async function main(...args) {
   await writeFile(packagePath, JSON.stringify(packageContent, null, 2) + '\n', {
     encoding: 'utf-8'
   });
+
+  autoDestroyConfArchetype();
+}
+
+const getPackageName = (config, scope, appName) => {
+  if (config.reset) return '@mercadona-fwk-front/arquetype';
+  return scope ? `${scope}/${appName}` : appName;
+}
+
+const autoDestroyConfArchetype = () => {
+  try {
+    FILES_TO_AUTODESTROY.forEach(fileToDelete => fs.unlinkSync(fileToDelete));  
+    DIRS_TO_AUTODESTROY.forEach(dirToDelete => fs.rmdirSync(dirToDelete));  
+  } catch (error) {
+    log(`Error when autodestroy scripts folder and content. Cause: ${error}`); 
+  }  
+}
+
+const sanitizeAppName = (appName) => {
+  const regex = /-front$/gm;
+  return regex.test(appName) ? appName.substr(0,appName.length - 6) :  appName;  
 }
 
 if (module === require.main) {
