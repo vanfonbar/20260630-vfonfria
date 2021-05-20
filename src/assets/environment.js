c44ff52f8bb1af7ed85ccfb3dd941c4301d861b4
fcampayo <fcampayo@mercadona.es>
2021-05-20 16:16:51 +0200

    feat: environment warnings (FWKA-3295)

--- src/assets/environment.js
+++ src/assets/environment.js
@@ -1,1 +1,5 @@
-/** Content for dynamic environment (immutable web) */
+/** IMPORTANTE: No modificar este fichero, debe estar vacío.
+ *  En los despliegues por los diferentes entornos se rellenará automáticamente
+ *  con las variables de entorno definidas en los ficheros YAML.
+ *  Cualquier contenido introducido a mano se borrará.
+ */
