1649abb675a9af42fcff5c79e0b7fef2192cd591
Javier Sánchez <jsanchor@mercadona.es>
2021-07-13 13:41:33 +0200

    docs: add warning about constants in app-config.constants.ts

--- src/app/app-config.constants.ts
+++ src/app/app-config.constants.ts
@@ -1,10 +1,12 @@
-// Constantes de la aplicación (variables que no cambian entre entornos)
+// IMPORTANTE: Las constantes definidas en este fichero definen la configuración inmutable de la aplicación.
+// El valor de las mismas no cambia con el entorno.
+// Siempre tienen el mismo valor independientemente del entorno en el que se ejecute la aplicación.
 import { AppConfig } from './app-config.interface';
 
 export const APP_CONFIG: AppConfig = {
   appName: 'yourAppName',
   language: {
     availableLanguages: ['es'],
-    defaultLanguage: 'es'
-  }
+    defaultLanguage: 'es',
+  },
 };
