e8d1adf052eeb2ee47a33db3e179f50d10cd371b
Jose Luis GM <jlgarcia@disid.com>
2021-09-24 08:48:35 +0200

    feat: Change logger app to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { MLoggerService } from '@mercadona/core/logger';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor(private mLoggerService: MLoggerService) {}
-
-  ngOnInit(): void {
-    this.mLoggerService.log('Welcome to FWK Front Angular Responsive');
-  }
+export class AppComponent {
+  constructor() {}
 }
--- src/app/pages/home/home.page-component.ts
+++ src/app/pages/home/home.page-component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IMAGES } from './embedded-images';
+import { MLoggerService } from '@mercadona/core/logger';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page-component.html',
   styleUrls: ['./home.page-component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
   images = IMAGES;
 
-  constructor() {}
+  constructor(private mLoggerService: MLoggerService) {}
+
+  ngOnInit(): void {
+    this.mLoggerService.log('Welcome to FWK Front Angular Responsive');
+  }
 
   goToUrl(url: string): void {
     window.open(url, '_blank');
