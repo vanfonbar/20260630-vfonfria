82c895fba71ded17eb26f24ebb9778bf366c0441
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2022-06-30 10:16:23 +0000

    feat: add new karma configuration for CICD (FWKA-7156)

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -2,11 +2,17 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  const webdriverConfig = {
+    hostname: 'selenium-hub.cloudbees-agents.svc.cluster.local',
+    port: 4444
+  };
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-webdriver-launcher'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-spec-reporter'),
@@ -58,11 +64,12 @@ module.exports = function (config) {
       }
     },
     reporters: ['kjhtml', 'spec', 'coverage'],
+    hostname: process.env.MY_POD_IP,
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadlessCI'],
+    browsers: ['ChromeHeadlessCI', 'GridChrome'],
     singleRun: true,
     browserNoActivityTimeout: 40000,
     restartOnFileChange: false,
@@ -73,6 +80,12 @@ module.exports = function (config) {
           '--no-sandbox' // required to run without privileges in docker
           // '--disable-web-security', // remove comment if we have CORS problems with local files (karma reports)
         ]
+      },
+      GridChrome: {
+        base: 'WebDriver',
+        config: webdriverConfig,
+        browserName: 'chrome',
+        pseudoActivityInterval: 30000
       }
     }
   });
