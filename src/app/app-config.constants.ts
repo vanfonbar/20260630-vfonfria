355ce73b62e09f49d7278494b60ede2bc0778624
Anabel Chacon Fernandez <achacofe@mercadona.es>
2023-09-14 11:03:28 +0200

    feat(app-config-constants): add new export constant on app-config (FWKA-9526)

--- src/app/app-config.constants.ts
+++ src/app/app-config.constants.ts
@@ -3,6 +3,7 @@
 // They always have the same value regardless of the environment in which the application is running.
 
 import { AppConfig } from './app-config.interface';
+import { MIconType } from '@mercadona/icons';
 
 // appName and appVersion will be replaced during CI execution
 export const APP_CONFIG: AppConfig = {
@@ -12,3 +13,9 @@ export const APP_CONFIG: AppConfig = {
     defaultLanguage: 'es'
   }
 };
+
+// IMPORTANT: Do not change the name of this constant
+// This constant is used when running the `npm run add:icons` script.
+// In case you want to use icons exported from another file, you will have to create an exported constant with the type `(MIconType | string)[]` and in addition to that
+// you will have to add them in this `ICONS_REGISTRY` constant.
+export const ICONS_REGISTRY: (MIconType | string)[][] = [];
