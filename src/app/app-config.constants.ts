db0df0d2b9b423d2dd3210fdc1d4c08bb1c16fae
Patricia Nieto <pnietoju@mercadona.com>
2023-11-21 12:41:06 +0100

    fix: new favicon(FWKA-9838)

--- src/app/app-config.constants.ts
+++ src/app/app-config.constants.ts
@@ -2,8 +2,9 @@
 // This values do not change with the environment.
 // They always have the same value regardless of the environment in which the application is running.
 
-import { AppConfig } from './app-config.interface';
 import { MIconType } from '@mercadona/icons';
+
+import { AppConfig } from './app-config.interface';
 
 // appName and appVersion will be replaced during CI execution
 export const APP_CONFIG: AppConfig = {
