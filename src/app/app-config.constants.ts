84eb62000c90fbb423a9b14dc9a96f0fc7ca65f7
Alberto Checa Muelas <alcheca@mercadona.es>
2024-03-27 08:55:35 +0100

    fix(app-config-constants): icons registry const and create new (FWKA-10306)

--- src/app/app-config.constants.ts
+++ src/app/app-config.constants.ts
@@ -1,12 +1,16 @@
-// IMPORTANT: Constants in this file are defining the immutable configuration of the application.
-// This values do not change with the environment.
-// They always have the same value regardless of the environment in which the application is running.
+/**
+ * IMPORTANT: Constants in this file are defining the immutable configuration of the application.
+ * These values do not change with the environment.
+ * They always have the same value regardless of the environment in which the application is running.
+ */
 
 import { MIconType } from '@mercadona/icons';
 
 import { AppConfig } from './app-config.interface';
 
-// appName and appVersion will be replaced during CI execution
+/**
+ * appName and appVersion will be replaced during CI execution
+ */
 export const APP_CONFIG: AppConfig = {
   appName: 'yourAppName',
   language: {
@@ -15,8 +19,33 @@ export const APP_CONFIG: AppConfig = {
   }
 };
 
-// IMPORTANT: Do not change the name of this constant
-// This constant is used when running the `npm run add:icons` script.
-// In case you want to use icons exported from another file, you will have to create an exported constant with the type `(MIconType | string)[]` and in addition to that
-// you will have to add them in this `ICONS_REGISTRY` constant.
-export const ICONS_REGISTRY: (MIconType | string)[][] = [];
+/**
+ * This constant is used as an array to add the icons and download them locally, example of use:
+ *
+ * export const ... = ['bold/interface-essential/remove-add/remove', 'bold/interface-essential/delete/delete-2', ...]
+ *
+ * After adding the icons in this way, it would be necessary to execute the instruction
+ * `npm run build:pro && npm run add:icons` after this in the `/dist/assets/mercadona` folder,
+ * the icons will be downloaded.
+ */
+export const ICONS_LIST: (MIconType | string)[] = [];
+
+/**
+ * This constant is the one that allows us to differentiate whether the icons will be read locally or through the bucket.
+ * By default, they will be read from the bucket (false) to read locally it must be changed to true.
+ */
+export const ICONS_LOCAL_MODE: boolean = true;
+
+/**
+ * IMPORTANT: Do not change the name of this constant because it is used to run the
+ * `npm run build:pro && npm run add:icons` schematic. This constant should only be
+ * used in case you have your icons grouped in arrays of strings, example:
+ *
+ * const icons1 = (MIconType | string) = ['bold/interface-essential/remove-add/remove']
+ * const icons2 = (MIconType | string) = ['bold/interface-essential/delete/delete-2']
+ *
+ * export const ... = [icons1, icons2]
+ *
+ * The icons will appear in the folder `/dist/assets/mercadona`.
+ */
+export const ICONS_REGISTRY: (MIconType | string)[][] = [ICONS_LIST];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { APP_CONFIG } from '@constants';
+import { APP_CONFIG, ICONS_LIST, ICONS_LOCAL_MODE } from '@constants';
 import { environment } from '@environment';
 
-import { MErrorHandlerModule } from '@mercadona/core-ui/error-handler';
-import { MPageErrorModule } from '@mercadona/core-ui/page-error';
-import { MPageNotFoundModule } from '@mercadona/core-ui/page-not-found';
 import { MLoggerModule } from '@mercadona/core/logger';
 import { MPlatformModule } from '@mercadona/core/platform';
 import { MTelemetryModule } from '@mercadona/core/telemetry';
 import { MTranslateModule } from '@mercadona/core/translate';
+import { MErrorHandlerModule } from '@mercadona/core-ui/error-handler';
+import { MPageErrorModule } from '@mercadona/core-ui/page-error';
+import { MPageNotFoundModule } from '@mercadona/core-ui/page-not-found';
+import { MIconModule } from '@mercadona/icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,6 +37,11 @@ import { AppComponent } from './app.component';
     MTelemetryModule.forRoot({
       url: environment.telemetryConfig.url,
       traces: environment.telemetryConfig.traces
+    }),
+    MIconModule.forRoot({
+      registry: 'folder',
+      icons: ICONS_LIST,
+      prodMode: ICONS_LOCAL_MODE
     })
   ],
   providers: [],
