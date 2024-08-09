4f2713374d129d607d32f6422ea2c9c818d5ed59
Mireia Malonda Mayor <mmalonda@mercadona.es>
2024-08-09 09:04:12 +0200

    test: add app component unit test (FWKA-10802)

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,42 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { APP_CONFIG } from '@constants';
+import { environment } from '@environment';
+import { MLoggerModule } from '@mercadona/core/logger';
+import { MPlatformModule } from '@mercadona/core/platform';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        MLoggerModule.forRoot({
+          logLevel: environment.logLevel
+        }),
+        MPlatformModule.forRoot({
+          appName: APP_CONFIG.appName
+        })
+      ],
+      declarations: [AppComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the AppComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should open a new window with the specified URL', () => {
+    spyOn(window, 'open');
+    const testUrl = 'https://example.com';
+    component.goToUrl(testUrl);
+    expect(window.open).toHaveBeenCalledWith(testUrl, '_blank');
+  });
+});
