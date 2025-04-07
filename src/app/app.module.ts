78ea3aa9da28c74026910cdf72dff96c14bae7d1
Francisco Montalt Leon <fmontalt@mercadona.es>
2025-04-07 16:04:32 +0200

    chore: change providers in app module  (FWKA-11471)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
 
 import { APP_CONFIG } from '@constants';
 import { environment } from '@environment';
 
-import { MLoggerModule } from '@mercadona/core/logger';
+import { provideMLogger } from '@mercadona/core/logger';
 import { MPlatformModule } from '@mercadona/core/platform';
 import { MTelemetryModule } from '@mercadona/core/telemetry';
 import { MTranslateModule } from '@mercadona/core/translate';
 import { MErrorHandlerModule } from '@mercadona/core-ui/error-handler';
 import { MPageErrorModule } from '@mercadona/core-ui/page-error';
-import { MPageNotFoundModule } from '@mercadona/core-ui/page-not-found';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,22 +22,17 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MErrorHandlerModule.forRoot(),
     MTranslateModule.forRoot(APP_CONFIG.language),
-    MLoggerModule.forRoot({
-      logLevel: environment.logLevel
-    }),
     MPlatformModule.forRoot({
       appName: APP_CONFIG.appName,
       environment: environment.env
     }),
-    MPageNotFoundModule,
     MPageErrorModule.forRoot(),
     MTelemetryModule.forRoot({
       url: environment.telemetryConfig.url,
       traces: environment.telemetryConfig.traces
-    }),
-    RouterModule.forRoot([])
+    })
   ],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
+  providers: [provideHttpClient(withInterceptorsFromDi()), provideMLogger(environment.logLevel)],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
