db051961ec910853648a106619908a6c25ade245
Francisco Montalt Leon <fmontalt@mercadona.es>
2025-05-05 10:33:07 +0200

    feat: change providers in app module (FWKA-11476)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,8 @@ import { provideMLogger } from '@mercadona/core/logger';
 import { MPlatformModule } from '@mercadona/core/platform';
 import { MTelemetryModule } from '@mercadona/core/telemetry';
 import { MTranslateModule } from '@mercadona/core/translate';
-import { MErrorHandlerModule } from '@mercadona/core-ui/error-handler';
-import { MPageErrorModule } from '@mercadona/core-ui/page-error';
+import { provideMErrorHandler } from '@mercadona/core-ui/error-handler';
+import { provideMPageError } from '@mercadona/core-ui/page-error';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,19 +20,22 @@ import { AppComponent } from './app.component';
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
-    MErrorHandlerModule.forRoot(),
     MTranslateModule.forRoot(APP_CONFIG.language),
     MPlatformModule.forRoot({
       appName: APP_CONFIG.appName,
       environment: environment.env
     }),
-    MPageErrorModule.forRoot(),
     MTelemetryModule.forRoot({
       url: environment.telemetryConfig.url,
       traces: environment.telemetryConfig.traces
     })
   ],
-  providers: [provideHttpClient(withInterceptorsFromDi()), provideMLogger(environment.logLevel)],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    provideMLogger(environment.logLevel),
+    provideMErrorHandler(),
+    provideMPageError()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
