1c046d6a8cd28ba01cc3778349d97053398fd315
Anabel Chacon Fernandez <achacofe@mercadona.es>
2025-06-13 15:14:28 +0200

    fix: add setup test standalone FWKA-11663

--- src/test.ts
+++ src/test.ts
@@ -6,6 +6,10 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
+import { setupDefaultTestProviders, setupPreventStandalone } from '@mercadona/components/common';
+
+setupDefaultTestProviders();
+setupPreventStandalone();
 document.body.classList.add('base-theme');
 
 // First, initialize the Angular testing environment.
